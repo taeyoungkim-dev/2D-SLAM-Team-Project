84a0a471a65bf5f7876b02c5c8cccbb342f9035a
taeyoungkim-dev <goldrunty@gmail.com>
2026-04-28 17:29:49 +0900

    Added ./assets/objects/red_ball.sdf ./assets/objects/blue_ball.sdf ./assets/objects/grean_ball.sdf ./assets/objects/door_blocker.sdf ./assets/objects/spawn_rgb_balls.launch.py
    Fixed
    README.md

--- assets/objects/spawn_rgb_balls.launch.py
+++ assets/objects/spawn_rgb_balls.launch.py
@@ -30,8 +30,17 @@ def generate_launch_description():
         output='screen'
     )
 
+    # 가벽(문 막이) 노드
+    spawn_blocker = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'blocker_1', '-file', os.path.join(base_dir, 'door_blocker.sdf'), '-x', '2.0', '-y', '0.0', '-z', '0.0'],
+        output='screen'
+    )
+
     return LaunchDescription([
         spawn_red,
         spawn_green,
-        spawn_blue
+        spawn_blue,
+        spawn_blocker
     ])
